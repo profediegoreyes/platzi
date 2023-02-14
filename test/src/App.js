b9e9cb534e6aea5618174f063a2d36e443f43437
profe_diego <profesor.diego.reyes@gmail.com>
2023-02-14 16:49:20 -0300

    2do

--- test/src/App.js
+++ test/src/App.js
@@ -8,10 +8,11 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          hola 
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://resactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
